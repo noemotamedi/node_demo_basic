e68587bf315a8c4da60107b501204086b44658ea
Noe <noemotamedi@students.berkeley.net>
2018-12-13 13:02:03 -0800

    first commit

---
+++ cat.js
@@ -0,0 +1,14 @@
+// grab the things we need
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+// create a schema
+var catSchema = new Schema({
+  name: String
+});
+
+//attach schema to model
+var Cat = mongoose.model('Cat', catSchema);
+
+// make this available to our users in our Node applications
+module.exports = Cat;
